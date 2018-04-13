763a161cdab2e22ec1a32809def77c2eadf17e86
Nick <nicholas.a.pawlawski@wmich.edu>
2018-04-13 12:59:53 -0400

    merge conflict

--- pages/connect.php
+++ pages/connect.php
@@ -2,11 +2,7 @@
 
 $user = 'root';
 $pass = '';
-<<<<<<< HEAD
 $conn = 'mysql:host=localhost;dbname=cs3500_imagedb';
-=======
-$conn = 'mysql:host=localhost;dbname=cs3500_ImageDB';
->>>>>>> b7cdb34d70707aa3f770e93445b0304368acd1ae
 
 $pdo = new PDO($conn, $user, $pass);
 
