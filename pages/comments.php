79bc037d19fb69c41eb9e4bf4f5e223c186b0d32
Austin Ragotzy <austin.p.ragotzy@wmich.edu>
2018-04-16 22:12:38 -0400

    adding some php to populate comments and replies i think it still needs to have ability to be sorted... which will be quick

--- pages/comments.php
+++ pages/comments.php
@@ -1,7 +1,4 @@
-<?php
-
-?>
-
+<?php  ?>
 <!DOCTYPE html>
 <html>
   <head>
@@ -15,35 +12,7 @@
         <h3>Comments</h3>
       </div>
       <div class="panel-body">
-        <details>
-          <summary class="commenter-row">
-            <div class="row cr">
-              <p><span class="commenter">Brosef6969: </span>this chair is wrecked bro!</p><br>
-            </div>
-            <div class="row cr">
-              <form class="" action="#" method="post">
-                <p>19</p>
-                <button type="submit" name="like" value="like"><span class="glyphicon glyphicon-thumbs-up"></span></button>
-                <p>4</p>
-                <button type="submit" name="dislike" value="dislike"><span class="glyphicon glyphicon-thumbs-down"></span></button>
-                <p class="pull-right">4 replies</p>
-              </form>
-            </div>
-          </summary>
-          <div class="container-fluid">
-            <div class="row replier-row">
-              <p class="reply"><span class="replier">ronald51:</span> go home brosef</p>
-            </div>
-          </div><br>
-          <form class="input-group reply" action="index.html" method="post">
-            <div class="form-group">
-              <input type="text" class="form-control" name="reply" rows="2" placeholder="what do YOU want to say to brosef6969"></input>
-            </div>
-              <div class="input-group-btn" style="height=100%;">
-                <button class="btn btn-default" type="submit" name=""><span class="glyphicon glyphicon-pencil"></span>Submit<span class="glyphicon glyphicon-pencil"></span></button>
-              </div>
-          </form>
-        </details><br>
+        <?php include 'php/popComments.php' ?>
         <form class="input-group" style="width: 100%;" action="index.html" method="post">
           <div class="form-group">
             <input type="text" class="form-control" name="comment" rows="2" placeholder="how do YOU really feel"></input>
---
+++ pages/php/popComments.php
@@ -0,0 +1,76 @@
+<?php
+//i will clean this up later hopefully
+include 'connect.php';
+try {
+  $sql = "SELECT * FROM Comments WHERE ImageID = ?";
+  $commentST = $pdo->prepare($sql);
+  $commentST->bindValue(1, $_GET['img']);
+  $commentST->execute();
+
+} catch (PDOException $e) {
+  die($e->getMessage());
+}
+//here is the break
+while($commentTup = $commentST->fetch()){
+  try {
+    $sql = "SELECT count(ReplyID) as replies FROM CommentReply WHERE CommentID = ?";
+    $numReplyST = $pdo->prepare($sql);
+    $numReplyST->bindValue(1, $commentTup['CommentID']);
+    $numReplyST->execute();
+
+    $sql = "SELECT * FROM CommentReply WHERE CommentID = ?";
+    $replyST = $pdo->prepare($sql);
+    $replyST->bindValue(1, $commentTup['CommentID']);
+    $replyST->execute();
+
+    $sql = "SELECT * FROM User WHERE UID = ?";
+    $commenterST = $pdo->prepare($sql);
+    $commenterST->bindValue(1, $commentTup['UID']);
+    $commenterST->execute();
+  } catch (PDOException $e) {
+    $e->getMessage();
+  }
+  $numRep = $numReplyST->fetch();
+  $commenterTup = $commenterST->fetch();
+  echo '<details>
+    <summary class="commenter-row">
+      <div class="row cr">
+        <p><span class="commenter">'.$commenterTup['Username'].': </span>'.$commentTup['Comment'].'</p><br>
+      </div>
+      <div class="row cr">
+        <form class="" action="#" method="post">
+          <p>'.$commentTup['Likes'].'</p>
+          <button type="submit" name="like" value="like"><span class="glyphicon glyphicon-thumbs-up"></span></button>
+          <p>'.$commentTup['Dislikes'].'</p>
+          <button type="submit" name="dislike" value="dislike"><span class="glyphicon glyphicon-thumbs-down"></span></button>
+          <p class="pull-right">'.$numRep['replies'].' replies</p>
+        </form>
+      </div>
+    </summary>';
+    while($replyTup = $replyST->fetch()){
+      try {
+        $sql = "SELECT * FROM User WHERE UID = ?";
+        $replierST = $pdo->prepare($sql);
+        $replierST->bindValue(1, $replyTup['UID']);
+        $replierST->execute();
+      } catch (PDOException $e) {
+        $e->getMessage();
+      }
+      $replierTup = $replierST->fetch();
+      echo '<div class="container-fluid">
+        <div class="row replier-row">
+          <p class="reply"><span class="replier">'.$replierTup['Username'].':</span> '.$replyTup['Comment'].'</p>
+        </div>
+      </div><br>';
+    }
+    echo '<form class="input-group reply" action="index.html" method="post">
+      <div class="form-group">
+        <input type="text" class="form-control" name="reply" rows="2" placeholder="what do YOU want to say to '.$commenterTup['Username'].'"></input>
+      </div>
+        <div class="input-group-btn" style="height=100%;">
+          <button class="btn btn-default" type="submit" name=""><span class="glyphicon glyphicon-pencil"></span>Submit<span class="glyphicon glyphicon-pencil"></span></button>
+        </div>
+    </form>
+  </details><br>';
+}
+ ?>
